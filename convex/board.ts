aebf6df92bfca18dc1bceb08cc9f7c487262a766
one_punch_man <ckim@banlaw.com>
2024-02-11 10:29:52 +1100

    Search Querying

--- convex/board.ts
+++ convex/board.ts
@@ -50,7 +50,18 @@ export const remove = mutation({
       throw new Error("Unauthorized");
     }
 
-    // TODO: later check to delete favorite relation as well
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", args.id)
+      )
+      .unique();
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
 
     await ctx.db.delete(args.id);
   },
@@ -140,9 +151,8 @@ export const unfavorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex(
-        "by_user_board",
-        (q) => q.eq("userId", userId).eq("boardId", board._id) // TODO: check if orgId needed
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
