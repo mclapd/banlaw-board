32cd7b99420533a6286ce733d508fbc0a6bd513d
one_punch_man <ckim@banlaw.com>
2024-02-10 15:38:36 +1100

    Empty States

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,28 @@
-const DashboardPage = () => {
-  return <div>Dashboard Root Page</div>;
+"use client";
+
+import { BoardList } from "./_components/board-list";
+import { EmptyOrg } from "./_components/empty-org";
+import { useOrganization } from "@clerk/nextjs";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
