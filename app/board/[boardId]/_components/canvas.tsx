a9b8fcf27d8fc324b3c117404cfb99cd7584befc
one_punch_man <ckim@banlaw.com>
2024-02-11 23:43:29 +1100

    Cursors Presence

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,12 +1,44 @@
 "use client";
 
-import { useState } from "react";
-import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useCallback, useMemo, useState, useEffect } from "react";
+import { LiveObject } from "@liveblocks/client";
 
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useHistory, useCanUndo, useCanRedo } from "@/liveblocks.config";
+import {
+  useHistory,
+  useCanUndo,
+  useCanRedo,
+  useMutation,
+  useStorage,
+  useOthersMapped,
+  useSelf,
+} from "@/liveblocks.config";
+
+import {
+  colorToCss,
+  connectionIdToColor,
+  findIntersectingLayersWithRectangle,
+  penPointsToPathLayer,
+  pointerEventToCanvasPoint,
+  resizeBounds,
+} from "@/lib/utils";
+
+import {
+  Camera,
+  CanvasMode,
+  CanvasState,
+  Color,
+  LayerType,
+  Point,
+  Side,
+  XYWH,
+} from "@/types/canvas";
+
+import { CursorsPresence } from "./cursors-presence";
+
+const MAX_LAYERS = 100;
 
 interface CanvasProps {
   boardId: string;
@@ -16,10 +48,58 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None,
   });
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+  const [lastUsedColor, setLastUsedColor] = useState<Color>({
+    r: 0,
+    g: 0,
+    b: 0,
+  });
 
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY,
+    }));
+  }, []);
+
+  const onPointerMove = useMutation(
+    ({ setMyPresence }, e: React.PointerEvent) => {
+      e.preventDefault();
+
+      const current = pointerEventToCanvasPoint(e, camera);
+
+      // if (canvasState.mode === CanvasMode.Pressing) {
+      //   startMultiSelection(current, canvasState.origin);
+      // } else if (canvasState.mode === CanvasMode.SelectionNet) {
+      //   updateSelectionNet(current, canvasState.origin);
+      // } else if (canvasState.mode === CanvasMode.Translating) {
+      //   translateSelectedLayers(current);
+      // } else if (canvasState.mode === CanvasMode.Resizing) {
+      //   resizeSelectedLayer(current);
+      // } else if (canvasState.mode === CanvasMode.Pencil) {
+      //   continueDrawing(current, e);
+      // }
+
+      setMyPresence({ cursor: current });
+    },
+    [
+      // continueDrawing,
+      camera,
+      canvasState,
+      // resizeSelectedLayer,
+      // translateSelectedLayers,
+      // startMultiSelection,
+      // updateSelectionNet,
+    ]
+  );
+
+  const onPointerLeave = useMutation(({ setMyPresence }) => {
+    setMyPresence({ cursor: null });
+  }, []);
 
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
@@ -33,6 +113,24 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+      <svg
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+      >
+        <g>
+          <CursorsPresence />
+          {/* {pencilDraft != null && pencilDraft.length > 0 && (
+            <Path
+              points={pencilDraft}
+              fill={colorToCss(lastUsedColor)}
+              x={0}
+              y={0}
+            />
+          )} */}
+        </g>
+      </svg>
     </main>
   );
 };
